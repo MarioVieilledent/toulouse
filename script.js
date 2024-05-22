61adcb1ea028c927d90df9d645939ca1e282fb8f
Mario <mario.vieilledent@solwr.com>
2024-05-22 14:57:40 +0200

    Set 3 commands, with eval to evaluate a js script

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 const token = process.env.CLIENT_TOKEN;
 
+const HELP = `
+- \`!help\` *List commands*
+- \`!ping\` *Pong*
+- \`!eval\` *Evaluates a JS script and write the returned value*
+- \`!eval\` *Evaluates a JS script and write the returned value*
+`;
+
+const timestampToDate = (ts) => {
+  const date = new Date(ts * 1000);
+  const hours = date.getHours();
+  const minutes = "0" + date.getMinutes();
+  const seconds = "0" + date.getSeconds();
+  return hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+};
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -19,16 +34,42 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (msg) => {
-  console.log("messageCreate => command " + msg.content);
+  const content = msg.content;
 
-  fs.writeFile("./msg_data.json", JSON.stringify(msg, true, 3), (err) => {
-    if (err) {
-      console.error(err);
+  console.log(
+    `${msg.author} ${timestampToDate(msg.createdTimestamp)} - ${content}`
+  );
+
+  if (msg.content.startsWith("!help")) {
+    msg.reply(HELP);
+  }
+
+  if (msg.content.startsWith("!ping")) {
+    msg.reply("Pong!");
+  }
+
+  if (msg.content.startsWith("!eval ")) {
+    const script = msg.content.slice(6);
+    try {
+      const interp = eval(script);
+      // Token disclosure protection
+      if (/[\w-]{24}\.[\w-]{6}\.[\w-]{27}/.test(interp)) {
+        msg.reply("Don't try to get the token u stinky whore");
+      } else {
+        switch (typeof interp) {
+          case "string":
+            msg.reply(interp);
+            break;
+          case "object":
+            msg.reply("```json\n" + JSON.stringify(interp, true, 3) + "```");
+            break;
+          default:
+            msg.reply(interp + "");
+        }
+      }
+    } catch (e) {
+      msg.reply("Error while evaluating code: " + e);
     }
-  });
-
-  if (msg.content.startsWith("!")) {
-    msg.reply("Commande " + msg.content);
   }
 });
 
