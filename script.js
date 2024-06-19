393f1337dcfd1bc3fc62f74809581d18725c608e
Mario <mario.vieilledent@solwr.com>
2024-06-19 14:58:35 +0200

    idk whatsdat

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ const token = process.env.CLIENT_TOKEN;
 const HELP = "!help";
 const PING = "!ping";
 const ECHO = "!echo";
+const LIFE = "!life";
 const EVAL = "!eval";
 
 const COMMAND_LIST = `
 - \`${HELP}\` *List commands*
 - \`${PING}\` *Pong*
 - \`${ECHO} <your shit here>\` *Stupidly repeats your message*
+- \`${LIFE}\` *Game of life*
 - \`${EVAL} <some js script>\` *Evaluates a JS script and write the returned value*
   - *e.g.* \`${EVAL} Math.sqrt(2)\`
   - *e.g.* \`${EVAL} [...Array(10)].reduce((a,_,i)=>a.concat(i>1?a[i-1]+a[i-2]:i),[]).join(', ')\`
@@ -33,6 +35,7 @@ const client = new Client({
     GatewayIntentBits.DirectMessages,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessageTyping,
   ],
   partials: ["MESSAGE", "CHANNEL"],
 });
@@ -72,6 +75,74 @@ client.on("messageCreate", (msgObj) => {
   if (msgObj.content.startsWith(ECHO)) {
     log(ECHO, content);
     msgObj.reply(msgObj.content.slice(6));
+  }
+
+  if (msgObj.content.startsWith(LIFE)) {
+    log(LIFE, content);
+    const x = 20;
+    const y = 10;
+
+    let msgToEdit;
+
+    let gol = Array(x).fill(Array(y).fill(false));
+
+    gol[5][3] = true;
+    gol[5][4] = true;
+    gol[5][5] = true;
+    gol[5][6] = true;
+    gol[5][7] = true;
+    gol[6][7] = true;
+
+    const toStr = (gol) =>
+      gol
+        .map((row) => row.map((cell) => (cell ? "■" : " ")).join(""))
+        .join("\n");
+
+    // Function to get the next state of the grid
+    const next = (gol) => {
+      let newGrid = Array.from({ length: x }, () => Array(y).fill(false));
+
+      // Helper function to count the live neighbors
+      const countLiveNeighbors = (gol, i, j) => {
+        let liveNeighbors = 0;
+        for (let di = -1; di <= 1; di++) {
+          for (let dj = -1; dj <= 1; dj++) {
+            if (di === 0 && dj === 0) continue;
+            const ni = i + di;
+            const nj = j + dj;
+            if (ni >= 0 && ni < x && nj >= 0 && nj < y && gol[ni][nj]) {
+              liveNeighbors++;
+            }
+          }
+        }
+        return liveNeighbors;
+      };
+
+      // Loop through each cell to determine the next state
+      for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++) {
+          const liveNeighbors = countLiveNeighbors(gol, i, j);
+          if (gol[i][j]) {
+            newGrid[i][j] = liveNeighbors === 2 || liveNeighbors === 3;
+          } else {
+            newGrid[i][j] = liveNeighbors === 3;
+          }
+        }
+      }
+
+      return newGrid;
+    };
+
+    for (let i = 0; i < 10; i++) {
+      msgObj.reply(toStr(gol)).then((elem) => {
+        console.log(Object.getOwnPropertyNames(elem));
+
+        setTimeout(() => {
+          gol = next(gol);
+          msgToEdit.edit(toStr(gol));
+        }, i * 300);
+      });
+    }
   }
 
   if (msgObj.content.startsWith(EVAL)) {
